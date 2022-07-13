1260b1c106762fb60ff0ce2384d796167dc845f5
Sebastian Kotarski <kotars01@stepstone.com>
2022-07-13 08:33:24 +0200

    Updated names

---
+++ DemoApp/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by kotars01 on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
